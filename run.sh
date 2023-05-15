d7fe09b696caf9ad31ad6331665a176592f8936b
dumble555 <>
2023-05-15 17:44:18 +0530

    update run.yml

--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
 chown -R $USER /github/home/.cache/pip
 chmod -R u+w /github/home/.cache/pip
 pip3 install requests lxml pycurl
-while read line; do
-  {
-    python3 tscr.py "$line"
-    exit_status=$?
-  } &
-
-  while true; do
-    echo "$line"
-    sleep 2
-  done | (
-    trap "kill 0" SIGINT SIGTERM EXIT;
-    wait
-  )
-
-  if [ $exit_status -eq 0 ]; then
-    echo "$line" | tee -a tsite-map-1.txt
-  fi
-done < tsite-map.txt
+while read line; do python3 tscr.py "$line" > /dev/null; echo "$line" | tee -a tsite-map-1.txt; done < tsite-map.txt
+#while read line; do
+#  {
+#    python3 tscr.py "$line"
+#    exit_status=$?
+#  } &
+#
+#  while true; do
+#    echo "$line"
+#    sleep 2
+#  done | (
+#    trap "kill 0" SIGINT SIGTERM EXIT;
+#    wait
+#  )
+#
+#  if [ $exit_status -eq 0 ]; then
+#    echo "$line" | tee -a tsite-map-1.txt
+#  fi
+#done < tsite-map.txt
